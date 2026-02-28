b80cedadcccc96f579354522ca03ecbe50b6cac4
kerelos aissam <kerelos2soliman004@gmail.com>
2026-02-28 20:21:20 +0200

    tagroba

--- lib/features/profile/views/my_profile_view.dart
+++ lib/features/profile/views/my_profile_view.dart
@@ -5,6 +5,10 @@ class MyProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text('My Profile'),
+      ),
+    );
   }
 }
