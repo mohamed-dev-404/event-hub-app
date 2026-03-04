bbde3a70f76c5e1098861d467edf4893f90e3f08
heba <heba.maged.5252449@gmail.com>
2026-03-04 03:29:40 +0200

    feat: add logo with title SVG and update asset references

--- lib/core/utils/assets/app_images.dart
+++ lib/core/utils/assets/app_images.dart
@@ -25,7 +25,8 @@ class AppImages {
   /// Assets for assetsImagesLogo
   /// assets/images/logo.svg
   static const String assetsImagesLogo = "assets/images/logo.svg";
-
+  static const String assetsImagesLogoWithTitle = "assets/images/logowithtitle.svg";
+  
   /// Assets for assetsImagesNotification
   /// assets/images/notification.png
   static const String assetsImagesNotification = "assets/images/notification.png";
