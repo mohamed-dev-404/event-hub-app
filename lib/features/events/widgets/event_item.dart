ed729b1cae1c9704e181ee0760cf4aee7bffa0dc
heba <heba.maged.5252449@gmail.com>
2026-03-04 08:42:58 +0200

    feat: add EventItem widget for displaying event details

---
+++ lib/features/events/widgets/event_item.dart
@@ -0,0 +1,85 @@
+import 'package:event_hub_app/core/utils/colors/app_colors.dart';
+import 'package:event_hub_app/core/utils/styles/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class EventItem extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String date;
+  final String location;
+  final VoidCallback? onTap;
+
+  const EventItem({
+    super.key,
+    required this.imagePath,
+    required this.title,
+    required this.date,
+    required this.location,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container
+      (
+        padding: const EdgeInsets.all(12),
+        margin: const EdgeInsets.only(bottom: 12),
+        decoration: BoxDecoration(
+          boxShadow: [BoxShadow(
+        color: Colors.black.withValues(alpha: .06), 
+        spreadRadius: 0,
+        blurRadius: 20,      
+        offset: const Offset(0, 4),
+      ),],
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          children: [
+            Image.asset(
+              imagePath,
+              width: 70,
+              height: 70,
+            ),
+            const SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    date,
+                    style: AppStyles.subTitle2
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    title,
+                    style: AppStyles.comment
+                  ),
+                  const SizedBox(height: 6),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.location_on,
+                        size: 14,
+                        color: AppColors.haifGray 
+                      ),
+                      const SizedBox(width: 4),
+                      Expanded(
+                        child: Text(
+                          location,
+                          style: AppStyles.title2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
