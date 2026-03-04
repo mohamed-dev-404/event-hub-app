556af19a44d12d7a1730e1d495952bd86942ef94
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-03-04 20:43:36 +0200

    fix: Refactor event view layout to use SingleChildScrollView and improve invite button interaction

--- lib/features/home/widgets/invite_friends_banner.dart
+++ lib/features/home/widgets/invite_friends_banner.dart
@@ -1,3 +1,4 @@
+import 'package:event_hub_app/core/functions/coming_soon_dialog.dart';
 import 'package:event_hub_app/core/utils/styles/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:event_hub_app/core/utils/colors/app_colors.dart';
@@ -52,20 +53,25 @@ class InviteFriendsBanner extends StatelessWidget {
                   ),
                   const SizedBox(height: 12),
 
-                  // Invite Button
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      color: AppColors.primaryCyanSecondary,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Text(
-                      'INVITE',
-                      style: AppStyles.subTitle1.copyWith(
-                        fontWeight: FontWeight.w900,
+                  //* Invite Button
+                  GestureDetector(
+                    onTap: () {
+                      comingSoonDialog(context);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
+                      decoration: BoxDecoration(
+                        color: AppColors.primaryCyanSecondary,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        'INVITE',
+                        style: AppStyles.subTitle1.copyWith(
+                          fontWeight: FontWeight.w900,
+                        ),
                       ),
                     ),
                   ),
